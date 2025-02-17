f4b3722f97f2ac3143364725a1e2dd7388d80d1e
Leereozk <tsvetkov.vadim.vyacheslavovich@gmail.com>
2025-02-17 13:34:56 +0300

    Fix building

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
+    server: {
+        allowedHosts: true,
+    },
     plugins: [
         react(),
         tailwindcss(),
